eb167b981d7e94386e0cf00a69dffa89a1c7ce76
kiran04-code <kr551344@gmail.com>
2025-06-07 00:45:41 +0530

    Fix import case: signin -> SignIn

--- vite-project/vite.config.js
+++ vite-project/vite.config.js
@@ -2,14 +2,26 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [react(), tailwindcss()],
   server: {
-    proxy:{
-      "/api":{
-        target:"https://mern-chatbot-3-1.onrender.com",
+    proxy: {
+      "/api": {
+        target: "https://mern-chatbot-3-1.onrender.com",
         changeOrigin: true,
-}    }
+      }
+    }
   },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            // Put all dependencies from node_modules in a vendor chunk
+            return 'vendor'
+          }
+        }
+      }
+    }
+  }
 })
